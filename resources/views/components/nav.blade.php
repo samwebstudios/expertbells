2d325fd70951e6ec94a966e57ec39ae0bdb2426a
Mithun Parihar <78896190+mithunparihar@users.noreply.github.com>
2022-12-06 19:03:58 +0530

    sd

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,7 +7,9 @@
                     <ul>
                         <li class="d-none d-lg-block"><a href="{{route('experts')}}">Find an Expert</a></li>
                         <li class="d-none d-lg-block"><a href="">Expert Services</a></li>
+                        @if(!\Auth::check() && !\Auth::guard('expert')->check())
                         <li class="d-none d-lg-block"><a href="{{route('login')}}" class="btn btn-thm m-0"><img src="{{asset('frontend/img/login.svg')}}" class="me-2" width="25" height="25"> Login</a></li>
+                        @endif
                         <li>
                             <button class="navbar-toggler collapsed menubar" type="button" data-bs-toggle="collapse" data-bs-target="#navigatin" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"><span><span class="bar"></span><span class="bar"></span><span class="bar"></span></span></button>
                             <div class="menu-bg"></div>
@@ -23,7 +25,9 @@
                                 </ul>
                             </div>
                         </li>
+                        @if(\Auth::check() || \Auth::guard('expert')->check())
                         <li><a href="account" title="account"><span><img src="{{asset('frontend/img/user1.svg')}}" width="36" height="36"></span></a></li>
+                        @endif
                         <li class="SearchBoxs">
                             <a data-bs-toggle="collapse" href="#Hsearch" role="button" aria-expanded="false" aria-controls="Search" title="Search"><span><img src="{{asset('frontend/img/search1.svg')}}" width="36" height="36"><!-- <i class="fal h5 fa-search m-0"></i> --></span></a>
                             <div id="Hsearch" class="collapse">
