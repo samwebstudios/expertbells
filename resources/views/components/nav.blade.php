c2fffaea39be7588ab7276535f62f4c487038653
Mithun Parihar <78896190+mithunparihar@users.noreply.github.com>
2022-12-14 19:02:48 +0530

    avdhesh

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -40,29 +40,29 @@
                             </a>
                             <div class="dropdown-menu NotiDrop">
                                 <ul>
-                                    <li class="active"><a href="http://localhost:8000/expert/dashboard"><span><i class="fal fa-tachometer-alt me-1"></i> Dashboard</span></a></li>
-                                    <li><a href="http://localhost:8000/expert/account"><span><i class="fal fa-user-edit me-1"></i> My Account</span></a></li>
-                                    <li><a href="#mycalls" class="collapsed" data-bs-toggle="collapse" aria-expanded="false" aria-controls="mycalls"><span><i class="fal fa-phone-rotary me-1"></i> My Calls</span> <i class="fal fa-chevron-down arrow"></i></a>
-                                        <ul class="collapse" id="mycalls">
-                                            <li><a href="http://localhost:8000/expert/schedules"><span><i class="fal fa-phone-alt me-1"></i> Scheduled Call</span></a></li>
+                                    <li class="{{Request::segment(2)=='dashboard'?'active':''}}"><a href="{{route('expert.dashboard')}}"><span><i class="fal fa-tachometer-alt me-1"></i> Dashboard</a></a></li>
+                                    <li class="{{Request::segment(2)=='account'?'active':''}}"><a href="{{route('expert.account')}}"><span><i class="fal fa-user-edit me-1"></i> My Account</a></a></li>
+                                    <li><a href="#menumycalls" class="collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse" aria-expanded="false" aria-controls="menumycalls"><span><i class="fal fa-phone-rotary me-1"></i> My Calls</span> <i class="fal fa-chevron-down arrow"></i></a>
+                                        <ul class="collapse" id="menumycalls">
+                                            <li><a href="{{route('expert.schedules')}}"><span><i class="fal fa-phone-alt me-1"></i> Scheduled Call</span></a></li>
                                             <li><a href="expert-account/closed-call.php"><span><i class="fal fa-phone-slash me-1"></i> Closed Call</span></a></li>
                                             <li><a href="expert-account/scheduled-calls.php"><span><i class="fal fa-phone-plus me-1"></i> Rescheduled Call</span></a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="http://localhost:8000/expert/slots"><span><i class="fal fa-tasks me-1"></i> Manage Slots</span></a></li>
+                                    <li class="{{Request::segment(2)=='slots'?'active':''}}"><a href="{{route('expert.slots')}}"><span><i class="fal fa-tasks me-1"></i> Manage Slots</span></a></li>
                                     <li><a href="expert-account/message.php"><span><i class="fal fa-comment-alt-lines me-1"></i> Message</span></a></li>
-                                    <li><a href="http://localhost:8000/expert/videos"><span><i class="fal fa-photo-video me-1"></i> My Video</span></a></li>
+                                    <li class="{{Request::segment(2)=='videos'?'active':''}}"><a href="{{route('expert.videos')}}"><span><i class="fal fa-photo-video me-1"></i> My Video</span></a></li>
                                     <li><a href="expert-account/my-review.php"><span><i class="fal fa-star me-1"></i> My Reviews</span></a></li>
                                     <li><a href="expert-account/reports.php"><span><i class="fal fa-file-contract me-1"></i> Reports</span></a></li>
                                     <li><a href="expert-account/help-center.php"><span><i class="fal fa-user-headset me-1"></i> Help Center</span></a></li>
-                                    <li><a href="http://localhost:8000/expert/expert-logout"><span><i class="fal fa-power-off me-1"></i> Logout</span></a></li>
+                                    <li><a href="{{route('expert.expertlogout')}}"><span><i class="fal fa-power-off me-1"></i> Logout</span></a></li>
                                 </ul>
                             </div>
                         </li>
                         @endif
                         @if(\Auth::check())
                         <li class="dropdown">
-                            <a href="{{route('user.dashboard')}}" title="{{userinfo()->name ?? ''}}">
+                            <a class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="{{route('user.dashboard')}}" title="{{userinfo()->name ?? ''}}">
                                 <span class="userimg">
                                     <x-image-box>
                                         <x-slot:image>{{userinfo()->profile}}</x-slot:image>
@@ -73,6 +73,24 @@
                                     </x-image-box>
                                 </span>
                             </a>
+                            <div class="dropdown-menu NotiDrop">
+                                <ul>
+                                    <li><a href="account/"><span><i class="fal fa-tachometer-alt me-1"></i> Dashboard</a></li>
+                                    <li><a href="account/my-account.php"><span><i class="fal fa-user-edit me-1"></i> My Account</a></li>
+                                    <li><a href="#Usermycalls" class="collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse" aria-expanded="false" aria-controls="Usermycalls"><span><i class="fal fa-phone-rotary me-1"></i> My Calls</span> <i class="fal fa-chevron-down arrow"></i></a>
+                                        <ul class="collapse" id="Usermycalls">
+                                            <li><a href="account/call-request.php"><span><i class="fal fa-phone-alt me-1"></i> Scheduled Call</span></a></li>
+                                            <li><a href="account/closed-call.php"><span><i class="fal fa-phone-slash me-1"></i> Closed Call</span></a></li>
+                                            <li><a href="account/call-request.php"><span><i class="fal fa-phone-plus me-1"></i> Rescheduled Call</span></a></li>
+                                            <li><a href="account/book-expert.php"><span><i class="fal fa-user-plus me-1"></i> Book Expert</span></a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="account/message.php"><span><i class="fal fa-comment-alt-lines me-1"></i> Message</span></a></li>
+                                    <li><a href="account/my-review.php"><span><i class="fal fa-star me-1"></i> My Reviews</span></a></li>
+                                    <li><a href="account/help-center.php"><span><i class="fal fa-user-headset me-1"></i> Help Center</a></li>
+                                    <li><a href="{{route('user.userlogout')}}"><span><i class="fal fa-power-off me-1"></i> Logout</a></li>
+                                </ul>
+                            </div>
                         </li>
                         @endif
                         <li class="SearchBoxs">
